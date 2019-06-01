c8ff431120ef21b0a2617f464470bb1bdc243451
Jenky Nolasco <jenky.nolasco@gmail.com>
2019-06-01 19:52:44 -0400

    initial commit

---
+++ babel.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  presets: [ 'module:metro-react-native-babel-preset' ],
+  plugins: [
+    'module-resolver',
+    {
+      root: [ './src' ],
+      alias: {},
+    },
+  ],
+}
